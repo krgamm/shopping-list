fdd3878ff2e9bcfd6bae6ecff70dbc79474c44e0
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2022-03-30 12:50:56 -0500

    updated gulp and dist

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,41 @@ const cssnano = require("cssnano");
 const babel = require("gulp-babel");
 const terser = require("gulp-terser");
 const browsersync = require("browser-sync").create();
+const gulp = require("gulp");
+
+function assetTask() {
+  return src("app/assets/**/*.+(png|jpg|gif|svg)").pipe(
+    gulp.dest("dist/images")
+  );
+}
+
+function htmlTask() {
+  return src("app/index.html", { sourcemaps: true }).pipe(
+    dest("dist", { sourcemaps: "." })
+  );
+}
 
 // Sass Task
 function scssTask() {
-  return src("src/scss/style.scss", { sourcemaps: true })
+  return src("app/scss/style.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(dest("dist", { sourcemaps: "." }));
+    .pipe(dest("dist/scss", { sourcemaps: "." }));
 }
 
 // JavaScript Task
 function jsTask() {
-  return src("src/js/main.js", { sourcemaps: true })
+  return src("app/js/main.js", { sourcemaps: true })
     .pipe(babel({ presets: ["@babel/preset-env"] }))
     .pipe(terser())
-    .pipe(dest("dist", { sourcemaps: "." }));
+    .pipe(dest("dist/js", { sourcemaps: "." }));
 }
 
 // Browsersync
 function browserSyncServe(cb) {
   browsersync.init({
     server: {
-      baseDir: ".",
+      baseDir: "./app",
     },
     notify: {
       styles: {
@@ -46,12 +59,19 @@ function browserSyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-  watch("*.html", browserSyncReload);
+  watch("./app/*.html", browserSyncReload);
   watch(
-    ["src/scss/**/*.scss", "src/**/*.js"],
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
     series(scssTask, jsTask, browserSyncReload)
   );
 }
 
 // Default Gulp Task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+exports.default = series(
+  assetTask,
+  htmlTask,
+  scssTask,
+  jsTask,
+  browserSyncServe,
+  watchTask
+);
